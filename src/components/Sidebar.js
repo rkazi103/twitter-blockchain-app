98f6406928034985f80970443a516e062f4450d1
Rayan <75285672+rkazi103@users.noreply.github.com>
2022-02-19 11:44:13 -0700

    feat: finished sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,110 @@
+import { RiHome7Line, RiHome7Fill, RiFileList2Fill } from "react-icons/ri";
+import { BiHash } from "react-icons/bi";
+import { FiBell, FiMoreHorizontal } from "react-icons/fi";
+import { HiOutlineMail, HiMail } from "react-icons/hi";
+import { FaRegListAlt, FaHashtag, FaBell } from "react-icons/fa";
+import { CgMoreO } from "react-icons/cg";
+import {
+  BsBookmark,
+  BsBookmarkFill,
+  BsPerson,
+  BsPersonFill,
+} from "react-icons/bs";
+import { VscTwitter } from "react-icons/vsc";
+import SidebarOption from "./SidebarOption";
+import { useState } from "react";
+
+const style = {
+  wrapper: `flex-[0.7] px-8 flex flex-col`,
+  twitterIconContainer: `text-3xl m-4`,
+  tweetButton: `bg-[#1d9bf0] hover:bg-[#1b8cd8] flex items-center justify-center font-bold rounded-3xl h-[50px] mt-[20px] cursor-pointer`,
+  navContainer: `flex-1`,
+  profileButton: `flex items-center mb-6 cursor-pointer hover:bg-[#333c45] rounded-[100px] p-2`,
+  profileLeft: `flex item-center justify-center mr-4`,
+  profileImage: `height-12 w-12 rounded-full`,
+  profileRight: `flex-1 flex`,
+  details: `flex-1`,
+  name: `text-lg`,
+  handle: `text-[#8899a6]`,
+  moreContainer: `flex items-center mr-2`,
+};
+
+const Sidebar = ({ initialSelectedIcon = "Home" }) => {
+  const [selected, setSelected] = useState(initialSelectedIcon);
+
+  return (
+    <div className="flex flex-[0.7] flex-col px-8">
+      <div className="m-4 text-3xl">
+        <VscTwitter />
+      </div>
+
+      <div className="flex-1">
+        <SidebarOption
+          Icon={selected === "Home" ? RiHome7Fill : RiHome7Line}
+          text="Home"
+          isActive={Boolean(selected === "Home")}
+          setSelected={setSelected}
+          redirect={"/"}
+        />
+        <SidebarOption
+          Icon={selected === "Explore" ? FaHashtag : BiHash}
+          text="Explore"
+          isActive={Boolean(selected === "Explore")}
+          setSelected={setSelected}
+        />
+        <SidebarOption
+          Icon={selected === "Notifications" ? FaBell : FiBell}
+          text="Notifications"
+          isActive={Boolean(selected === "Notifications")}
+          setSelected={setSelected}
+        />
+        <SidebarOption
+          Icon={selected === "Messages" ? HiMail : HiOutlineMail}
+          text="Messages"
+          isActive={Boolean(selected === "Messages")}
+          setSelected={setSelected}
+        />
+        <SidebarOption
+          Icon={selected === "Bookmarks" ? BsBookmarkFill : BsBookmark}
+          text="Bookmarks"
+          isActive={Boolean(selected === "Bookmarks")}
+          setSelected={setSelected}
+        />
+        <SidebarOption
+          Icon={selected === "Lists" ? RiFileList2Fill : FaRegListAlt}
+          text="Lists"
+          isActive={Boolean(selected === "Lists")}
+          setSelected={setSelected}
+        />
+        <SidebarOption
+          Icon={selected === "Profile" ? BsPersonFill : BsPerson}
+          text="Profile"
+          isActive={Boolean(selected === "Profile")}
+          setSelected={setSelected}
+          redirect={"/profile"}
+        />
+        <SidebarOption Icon={CgMoreO} text="More" setSelected={setSelected} />
+
+        <div className="mt-[20px] flex h-[50px] cursor-pointer items-center justify-center rounded-3xl bg-[#1d9bf0] font-bold hover:bg-[#1b8cd8]">
+          Mint
+        </div>
+      </div>
+
+      <div className="mb-6 flex cursor-pointer items-center rounded-[100px] p-2 hover:bg-[#333c45]">
+        <div className="item-center mr-4 flex justify-center"></div>
+        <div className="flex flex-1">
+          <div className="flex-1">
+            <div className="text-lg">rkazi103</div>
+            <div className="text-[#8899a6]">@0x22dF...4xf2rf</div>
+          </div>
+
+          <div className="mr-2 flex items-center">
+            <FiMoreHorizontal />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/SidebarOption.js
@@ -0,0 +1,27 @@
+import { Dispatch, SetStateAction } from "react";
+import { IconType } from "react-icons";
+import { useRouter } from "next/router";
+
+const style = {
+  wrapper: `w-min flex items-center rounded-[100px] p-4 cursor-pointer hover:bg-[#333c45] transition-all hover:duration-200 hover:ease-in-out`,
+  iconContainer: `text-xl mr-4`,
+  textGeneral: `font-medium`,
+  textActive: `font-bold`,
+};
+
+const SidebarOption = ({ text, Icon, isActive, setSelected, redirect }) => {
+  return (
+    <div
+      className="flex w-min cursor-pointer items-center rounded-[100px] p-4 transition-all hover:bg-[#333c45] hover:duration-200 hover:ease-in-out"
+      onClick={() => setSelected(text)}
+    >
+      <div className="mr-4 text-xl">
+        <Icon />
+      </div>
+
+      <div className={`${isActive ? "font-bold" : "font-medium"}`}>{text}</div>
+    </div>
+  );
+};
+
+export default SidebarOption;
