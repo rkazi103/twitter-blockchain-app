1f20d649766e38d729365ac912d22499c506f4fc
Rayan <75285672+rkazi103@users.noreply.github.com>
2022-02-20 14:41:21 -0700

    feat: deployed smart contract

---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+import contractArtifact from "../artifacts/contracts/MintProfileImage.sol/ProfileImageNfts.json";
+
+export const contractAddress = "0xE41BCfA6AdEfD5A829c93c1B835D925cb0461Ddf";
+export const contractABI = contractArtifact.abi;
