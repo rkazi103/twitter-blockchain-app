a979a850d584c2c826bb40d246089d43494c08b3
Rayan <75285672+rkazi103@users.noreply.github.com>
2022-02-20 14:24:37 -0700

    chore: created new hardhat project

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+require("@nomiclabs/hardhat-waffle");
+require("solidity-coverage");
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  paths: {
+    artifacts: "../src/artifacts",
+  },
+};
