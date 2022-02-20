9835fc1998799a6cb5f77ec02135d442db07caa1
Rayan <75285672+rkazi103@users.noreply.github.com>
2022-02-19 19:19:48 -0700

    fix: hid scrollbar

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,15 +1,9 @@
 import { BsStars } from "react-icons/bs";
 import TweetBox from "./TweetBox";
 
-const style = {
-  wrapper: `flex-[2] border-r border-l border-[#38444d] overflow-y-scroll`,
-  header: `sticky top-0 bg-[#15202b] z-10 p-4 flex justify-between items-center`,
-  headerTitle: `text-xl font-bold`,
-};
-
 const Feed = () => {
   return (
-    <div className="flex-[2] overflow-y-scroll border-r border-l border-[#38444d]">
+    <div className="flex-[2] overflow-y-scroll border-r border-l border-[#38444d] scrollbar-hide">
       <div className="sticky top-0 z-10 flex items-center justify-between bg-[#15202b] p-4">
         <div className="text-xl font-bold">Home</div>
         <BsStars />
