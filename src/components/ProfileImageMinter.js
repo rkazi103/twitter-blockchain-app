6337451314e4210c030659bec80dc6ccb6bc8f0e
Rayan <75285672+rkazi103@users.noreply.github.com>
2022-02-21 14:27:10 -0700

    feat: added render functionality for minter

--- src/components/ProfileImageMinter.js
+++ src/components/ProfileImageMinter.js
@@ -1,9 +1,50 @@
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useState, useContext } from "react";
+import TwitterContext from "../context/TwitterContext";
+import InitialMintingState from "./InitialMintingState";
+import LoadingMintingState from "./LoadingMintingState";
+import FinishedMintingState from "./FinishedMintingState";
 
 const ProfileImageMinter = () => {
-  const [status, setStatus] = useState("initial");
+  const { currentAccount, setAppStatus } = useContext(TwitterContext);
+  const router = useRouter();
 
-  return <div>ProfileImageMinter</div>;
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [status, setStatus] = useState("initial");
+  const [profileImage, setProfileImage] = useState();
+
+  const mint = async () => {};
+
+  const renderLogic = (modalStatus = status) => {
+    switch (modalStatus) {
+      case "initial":
+        return (
+          <InitialMintingState
+            profileImage={profileImage}
+            setProfileImage={setProfileImage}
+            name={name}
+            setName={setName}
+            description={description}
+            setDescription={setDescription}
+            mint={mint}
+          />
+        );
+
+      case "loading":
+        return <LoadingMintingState />;
+
+      case "finished":
+        return <FinishedMintingState />;
+
+      default:
+        router.push("/");
+        setAppStatus("error");
+        break;
+    }
+  };
+
+  return <>{renderLogic()}</>;
 };
 
 export default ProfileImageMinter;
