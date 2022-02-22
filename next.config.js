528d504ab0239a37f1b536dc55f6b48c11cadb98
Rayan <75285672+rkazi103@users.noreply.github.com>
2022-02-21 20:54:08 -0700

    feat: added pwa functionality

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withPWA = require("next-pwa");
 
-module.exports = nextConfig
+const nextConfig = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+});
+
+module.exports = nextConfig;
